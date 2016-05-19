67dfb43abc45b599c6a42b2b7305ae972aeb9ae3
Evgeny Popov <evgeny.popov@eastbanctech.com>
2016-05-19 00:49:43 -0700

    test

--- src/client/IApiConfig.ts
+++ src/client/IApiConfig.ts
@@ -2,4 +2,5 @@ export interface IApiConfig {
     basePath: string;
     defaultHeaders: any;
     timeout: number;
+    token: string;
 }
--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from './api/api';
 export * from './model/models';
 export {IApiConfig} from './client/IApiConfig';
 export {AuthorizationService} from './client/AuthorizationService';
+
